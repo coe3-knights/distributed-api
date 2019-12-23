7c7f3efd8e4468e54c96086ebab95a853d6f8a0f
cbenisson <35075194+cbenisson@users.noreply.github.com>
2019-12-23 10:06:24 +0000

    Update LibraryController.php

--- app/controllers/LibraryController.php
+++ app/controllers/LibraryController.php
@@ -24,12 +24,11 @@ class LibraryController extends Controller{
    */
     public function uploadAction(){ 
     	//checks for user request method
-	    echo json_encode(array("message"=>"reached here"));
-	     die('her3');
+	  
         if(Method::isPost()){
               $register_vals = $_POST;
-	      
-	     
+	      var_dump($register_vals); die();
+		
               $file_type = $_FILES['file']['type'];
               $file_val = file_get_contents($_FILES['file']['tmp_name']);
               $register_vals['data'] = base64_encode($file_val);
